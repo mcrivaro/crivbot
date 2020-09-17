56022efdf8b222d5a4d45605231a97a48bd0d547
mcrivaro <marcocrivaro@gmail.com>
2020-09-17 18:05:15 +0200

    fix message parsing

--- crivbot.py
+++ crivbot.py
@@ -1,4 +1,4 @@
-from requests import Request
+import requests
 from flask import Flask, request
 import os
 import json
@@ -13,25 +13,27 @@ except:
 # telegram_token = app.config['TOKEN']
 telegram_uri = f'https://api.telegram.org/Criv_Bot:{telegram_token}/sendMessage'
 
+
 @app.route('/')
 def hello_world():
     return 'This is CrivBot!'
 
+
 @app.route('/new-message', methods=['POST'])
 def new_message():
-    message = json.loads(request.data)
-    print(str(message))
+    message = json.loads(request.data)['message']
     body = {
         'chat_id': message['chat']['id'],
         'text': 'Hi From CrivBot!'
     }
     try:
-        req = Request(method='POST',url=telegram_uri,data=json.dumps(body))
-        print('message sent')
+        res = requests.post(url=telegram_uri,
+                            data=json.dumps(body))
+        print(f'message sent. status {res}')
     except:
         print('error while sending message')
     return message
-    
 
-if __name__ == "__main__":    
-    app.run(host='0.0.0.0',port=port)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
