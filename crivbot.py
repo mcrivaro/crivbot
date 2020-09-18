0f9ec1bfa591d5ed5f1c65e309a023f8dcbf0f41
mcrivaro <marcocrivaro@gmail.com>
2020-09-18 22:27:31 +0200

    fix syntax error

--- crivbot.py
+++ crivbot.py
@@ -25,7 +25,7 @@ def new_message():
     print(message_complete_data)
     message = message_complete_data['message']
     chat_id = message['chat']['id']
-    text = message['text'] if hasattr(message, 'text') else: None
+    text = message['text'] if hasattr(message, 'text') else None
     try:
         action = Action(telegram_uri, chat_id, text)
         action.evaluate()
